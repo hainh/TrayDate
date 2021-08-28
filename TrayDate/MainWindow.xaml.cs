4098aebb1b864fd244637f82377e507822eacd4f
Hai Nguyen <hainh.vn@gmail.com>
2021-08-28 21:03:05 +0700

    * Initial commit

---
+++ TrayDate/MainWindow.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace TrayDate
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private readonly System.ComponentModel.IContainer components = null;
+        private readonly NotifyIcon notifyIcon;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            components = new System.ComponentModel.Container();
+            notifyIcon = new NotifyIcon(components);
+            notifyIcon.Click += NotifyIcon_Click;
+            notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+            notifyIcon.Visible = false;
+            CreateTextIcon();
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            try
+            {
+                string[] lines = System.IO.File.ReadAllLines(stateFilePath);
+                textBox1.Text = lines[0];
+                textBox2.Text = lines[1];
+            }
+            catch { }
+        }
+
+        private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+        {
+            _ = Task.Run(async () =>
+            {
+                await Task.Delay(500);
+                Dispatcher.Invoke(() =>
+                {
+                    Show();
+                    notifyIcon.Visible = false;
+                });
+            });
+        }
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            Hide();
+            CreateTextIcon();
+            notifyIcon.Visible = true;
+        }
+
+        private readonly string stateFilePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "TrayDate.txt");
+        private void SaveState()
+        {
+            try
+            {
+                System.IO.File.WriteAllLines(stateFilePath, new string[] { textBox1.Text, textBox2.Text });
+            }
+            catch { }
+        }
+
+        private void CreateTextIcon()
+        {
+            Rect screen = WpfScreen.GetScreenFrom(this).DeviceBounds;
+            double width = SystemParameters.FullPrimaryScreenWidth;
+            int iconWidth = (int)(SystemParameters.SmallIconWidth * screen.Width / width);
+
+            string str = DateTime.Now.ToString(textBox1.Text);
+            int fontHeight = (int)Math.Round(iconWidth * 6 / 7.0);
+            Font fontToUse = new Font("Tahoma", fontHeight, System.Drawing.FontStyle.Regular, GraphicsUnit.Pixel);
+            Brush brushToUse = new SolidBrush(Color.White);
+            Bitmap bitmapText = new Bitmap(iconWidth, iconWidth);
+            Graphics g = Graphics.FromImage(bitmapText);
+
+            IntPtr hIcon;
+            SizeF size = g.MeasureString(str, fontToUse);
+
+            g.Clear(Color.Transparent);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            g.DrawString(str, fontToUse, brushToUse, (bitmapText.Width - size.Width) / 2, (bitmapText.Height - size.Height) / 2);
+            Brush lineBrush = new SolidBrush(Color.CadetBlue);
+            Pen pen = new Pen(lineBrush);
+            int x = bitmapText.Width - 1, y = bitmapText.Height - 1;
+            int d = (bitmapText.Width + bitmapText.Height) / 8;
+            g.DrawRectangle(pen, 0, 0, x, y);
+            g.DrawLine(pen, x - d, y, x, y - d);
+            g.DrawLine(pen, x - d + 1, y, x, y - d + 1);
+            hIcon = bitmapText.GetHicon();
+            notifyIcon.Icon = System.Drawing.Icon.FromHandle(hIcon);
+            notifyIcon.Text = DateTime.Now.ToString(textBox2.Text);
+
+            SaveState();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Hide();
+            CreateTextIcon();
+            notifyIcon.Visible = true;
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            Title = "Show Date";
+        }
+
+        private void Window_Deactivated(object sender, EventArgs e)
+        {
+            Title = DateTime.Now.ToString(textBox2.Text);
+            SaveState();
+        }
+    }
+}
---
+++ TrayDate/WpfScreen.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Interop;
+
+namespace TrayDate
+{
+    public class WpfScreen
+    {
+        public static IEnumerable<WpfScreen> AllScreens()
+        {
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                yield return new WpfScreen(screen);
+            }
+        }
+
+        public static WpfScreen GetScreenFrom(Window window)
+        {
+            WindowInteropHelper windowInteropHelper = new WindowInteropHelper(window);
+            Screen screen = Screen.FromHandle(windowInteropHelper.Handle);
+            WpfScreen wpfScreen = new WpfScreen(screen);
+            return wpfScreen;
+        }
+
+        public static WpfScreen GetScreenFrom(System.Windows.Point point)
+        {
+            int x = (int)Math.Round(point.X);
+            int y = (int)Math.Round(point.Y);
+
+            // are x,y device-independent-pixels ??
+            System.Drawing.Point drawingPoint = new System.Drawing.Point(x, y);
+            Screen screen = Screen.FromPoint(drawingPoint);
+            WpfScreen wpfScreen = new WpfScreen(screen);
+
+            return wpfScreen;
+        }
+
+        public static WpfScreen Primary => new WpfScreen(Screen.PrimaryScreen);
+
+        private readonly Screen screen;
+
+        internal WpfScreen(Screen screen)
+        {
+            this.screen = screen;
+        }
+
+        public Rect DeviceBounds => GetRect(screen.Bounds);
+
+        public Rect WorkingArea => GetRect(screen.WorkingArea);
+
+        private Rect GetRect(Rectangle value)
+        {
+            // should x, y, width, height be device-independent-pixels ??
+            return new Rect
+            {
+                X = value.X,
+                Y = value.Y,
+                Width = value.Width,
+                Height = value.Height
+            };
+        }
+
+        public bool IsPrimary => screen.Primary;
+
+        public string DeviceName => screen.DeviceName;
+    }
+
+}
